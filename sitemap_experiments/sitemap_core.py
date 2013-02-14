5f3f566579e55c9132d30d122a487b45f4e83502
Shriphani Palakodety <shriphanip@gmail.com>
2013-02-13 19:41:47 -0500

    html reports implemented

--- sitemap_experiments/sitemap_core.py
+++ sitemap_experiments/sitemap_core.py
@@ -31,25 +31,28 @@ def handle_warc_file(warc_file_name):
 		return
 
 	if PROCESS_STDOUT_FILENAME is None:
-		initialize_output_files(id(multiprocessing.current_process()))
+		initialize_output_files(0)
 
 	w = warc.open(warc_file_name)
 
-	for record in w:
-		try:
-			record_url_parsed = urlparse.urlparse(record['WARC-Target-URI'])
+	try:
+		for record in w:
+			try:
+				record_url_parsed = urlparse.urlparse(record['WARC-Target-URI'])
 
-			host = record_url_parsed.hostname
-			queries_string = record_url_parsed.query
+				host = record_url_parsed.hostname
+				queries_string = record_url_parsed.query
 
-			queries = [x.split('=')[0] for x in queries_string.split('&')] # grabs a list of keys
+				queries = [x.split('=')[0] for x in queries_string.split('&')] # grabs a list of keys
 
-			for query in queries:
-				print host, query # one line per host and query combination
-		except:
-			sys.stderr.write('fuck_up')
-	print warc_file_name
+				for query in queries:
+					print host, query # one line per host and query combination
+			except:
+				sys.stderr.write('Awkward record in ' + warc_file_name + '\n')
+				continue
 
+	except IOError:
+		sys.stderr.write('Awkward record found in ' + warc_file_name + '\n')
 
 def parse_cmdline_args():
 	parser = argparse.ArgumentParser()
@@ -64,6 +67,7 @@ def parse_cmdline_args():
 
 	return parser.parse_args()
 
+
 if __name__ == '__main__':
 	parsed = parse_cmdline_args()
 
@@ -76,6 +80,8 @@ if __name__ == '__main__':
 	reload(sys)
 	sys.setdefaultencoding('utf-8')
 
+	sys.stderr = open('sitemaps.err', 'w')
+
 	jobs_handlers_pool = multiprocessing.Pool(PROCS_POOL_SIZE)
 
 	for root, dirs, files in os.walk(parsed.job_directory):
@@ -83,10 +89,8 @@ if __name__ == '__main__':
 		if not root.find('warcs') >= 0:
 			continue
 
-		jobs_handlers_pool.map(
-			handle_warc_file,
-			[os.path.join(root, f) for f in files]
-		)
+		for filename in files:
+			handle_warc_file(os.path.join(root, filename))
 
 	# at the end of this job, cat all the output files and process it further
 
@@ -108,4 +112,4 @@ if __name__ == '__main__':
 		final_no_queries_handle.write(line + '\n')
 
 	# remove the intermediate files generated
-	os.system('rm SEEDS_QUERIES_MAP_*')
+	#os.system('rm SEEDS_QUERIES_MAP_*')
